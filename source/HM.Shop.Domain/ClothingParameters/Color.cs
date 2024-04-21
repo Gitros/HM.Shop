ad0dca3ec649eb1d8d01bbbdb3ab32342b67fb54
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-21 14:58:47 +0200

    test

---
+++ source/HM.Shop.Domain/ClothingParameters/Color.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM.Shop.Domain.ClothingParameters;
+
+internal class Color
+{
+}
---
+++ source/HM.Shop.Domain/Skirts/Skirt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM.Shop.Domain.Skirts;
+
+
+internal class Skirt
+{
+}
